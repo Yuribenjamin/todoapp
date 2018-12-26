a7e0fdaa1611610d9f075cc0b2040d90f9112f62
Abraham Ragab <Abrahammoustafa@hotmail.co.uk>
2018-12-26 17:30:21 +0200

    Updates

---
+++ precache-manifest.fe13197f93b178eefd5bcc0971907c7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b30c89f660ab15f5723d662f79f5dae6",
+    "url": "/todoapp/static/media/error.b30c89f6.svg"
+  },
+  {
+    "revision": "98d74c387b18273e9846c95d1416e0e5",
+    "url": "/todoapp/static/media/add.98d74c38.svg"
+  },
+  {
+    "revision": "ca8789d6bb6dac684f74",
+    "url": "/todoapp/static/js/runtime~main.ca8789d6.js"
+  },
+  {
+    "revision": "8a718805f66077f66487",
+    "url": "/todoapp/static/js/main.8a718805.chunk.js"
+  },
+  {
+    "revision": "9d65a0d5e5a889113337",
+    "url": "/todoapp/static/js/1.9d65a0d5.chunk.js"
+  },
+  {
+    "revision": "8a718805f66077f66487",
+    "url": "/todoapp/static/css/main.a33b09b3.chunk.css"
+  },
+  {
+    "revision": "dd57d57c0e1ac40f8af291599e3dd98d",
+    "url": "/todoapp/index.html"
+  }
+];
